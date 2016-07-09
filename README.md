81a59d9429e2f2611ed9196f7163876796bd48c5
Kyle J. Stiemann <stiemannkj1@gmail.com>
2016-07-09 14:21:04 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,37 @@
+# Example Git Repository
+
+This project is an example `git` repository for my [*Version Control*](http://stiemannkj1.github.io/version-control/)
+blog post.
+
+## Get-Day Program
+
+This program outputs the day of the week for the given date string.
+
+## Building the project
+
+To build the project:
+
+1. Clone the project:
+
+        git clone https://github.com/stiemannkj1/example-git-project.git
+
+2. Build the project with [`gradle`](https://gradle.org/):
+
+        gradle build
+
+The jar can be found under **`build/libs`**.
+
+## Running the Jar
+
+You can run the `jar` from the command line:
+
+    java -jar build/libs/get-day.jar 2016-07-09
+
+Output:
+
+    SATURDAY
+
+# License
+
+This software is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+
---
+++ src/main/java/io/github/stiemannkj1/example/git/project/GetDayProgram.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2016 Kyle Stiemann
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package io.github.stiemannkj1.example.git.project;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+/**
+ * @author Kyle Stiemann
+ */
+public class GetDayProgram {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.err.println("Error: get-day requires one date string argument of the format: yyyy-MM-dd.");
+            System.exit(1);
+        } else if (args.length > 1) {
+            System.out.println("Warning: get-day only accepts one argument. Ignoring all arguments after " + args[0]);
+        }
+
+        String dateString = args[0];
+
+        try {
+            LocalDate date = LocalDate.parse(dateString);
+            System.out.println(date.getDayOfWeek());
+        } catch (DateTimeParseException e) {
+            System.err.println("Error: date string must be of the format: yyyy-MM-dd.");
+            System.exit(1);
+        }
+
+        System.exit(0);
+    }
+}
